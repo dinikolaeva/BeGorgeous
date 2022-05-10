b4f679c8c478618fb45bb977ca85380f8adb1aed
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2022-05-10 12:33:39 +0300

    Add changes in some files and deployment files

--- Web/BeGorgeous.Web/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ Web/BeGorgeous.Web/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text.Encodings.Web;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using BeGorgeous.Data.Models;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.WebUtilities;
-
-namespace BeGorgeous.Web.Areas.Identity.Pages.Account
+﻿namespace BeGorgeous.Web.Areas.Identity.Pages.Account
 {
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+    using System.Text.Encodings.Web;
+    using System.Threading.Tasks;
+
+    using BeGorgeous.Data.Models;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.UI.Services;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.AspNetCore.WebUtilities;
+
     [AllowAnonymous]
     public class ForgotPasswordModel : PageModel
     {
